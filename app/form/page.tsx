915017d8a6e0997bde9adaf7a28a1c6bc6751871
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-17 21:26:00 +0500

    changes

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,44 +1,29 @@
-// components/ContactForm.tsx
-"use client"
 import React from 'react';
 
 const ContactForm: React.FC = () => {
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const formData = new FormData(form);
-
-    try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData as any).toString()
-      });
-      alert('Form successfully submitted');
-    } catch (error) {
-      alert('Form submission error');
-    }
-  };
+  
 
   return (
-    <form className='text-red-500' name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
-      <input type="hidden" name="form-name" value="contact" />
-      <p hidden>
-        <label>Don’t fill this out: <input name="bot-field" /></label>
-      </p>
-      <p>
-        <label>Namesss: <input type="text" name="name" required /></label>
-      </p>
-      <p>
-        <label>Email: <input type="email" name="email" required /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message" required></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+
+    <p>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" />
+    </p>
+    <p>
+      <label htmlFor="email">Email</label>
+      <input type="text" id="email" name="email" />
+    </p>
+    <p>
+      <label htmlFor="message">Message</label>
+      <textarea id="message" name="message"></textarea>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+
   );
 };
 
