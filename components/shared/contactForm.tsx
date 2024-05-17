0d1d7be7d3bdeae0cf5d932716def71be94517d8
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-18 00:07:58 +0500

    chnges

--- components/shared/contactForm.tsx
+++ components/shared/contactForm.tsx
@@ -1,36 +1,36 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import SectionTitle from "./sectionTitle";
 import ColoredSpan from "./coloredSpan";
 import HeadingDescription from "./headingDescription";
 
 function ContactForm() {
-    const [status, setStatus] = useState("");
-    const [error, setError] = useState("");
+  const [status, setStatus] = useState("");
+  const [error, setError] = useState("");
 
-    const handleFormSubmit = async (event : any) => {
-        event.preventDefault();
-        try {
-            setStatus('pending');
-            setError("error");
-            const myForm = event.target;
-            const formData = new FormData(myForm);
-            const res = await fetch('/__forms.html', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams(formData.toString()).toString()
-            });
-            if (res.status === 200) {
-                setStatus('ok');
-            } else {
-                setStatus('error');
-                setError(`${res.status} ${res.statusText}`);
-            }
-        } catch (e) {
-            setStatus('error');
-            setError(`${e}`);
-        }
-    };
+  const handleFormSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      setStatus("pending");
+      setError("error");
+      const myForm = event.target;
+      const formData = new FormData(myForm);
+      const res = await fetch("../public/__forms.html", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData.toString()),
+      });
+      if (res.status === 200) {
+        setStatus("ok");
+      } else {
+        setStatus("error");
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setStatus("error");
+      setError(`${e}`);
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 gap-5 place-content-center h-full lg:px-16 px-3">
@@ -45,30 +45,43 @@ function ContactForm() {
       </HeadingDescription>
 
       <form
-                    name="contact"
-                    method="POST" data-netlify="true"
-                    onSubmit={handleFormSubmit}
-                    className="text-black flex flex-col gap-3 align-center"
-                >
-                    <input type="hidden" name="form-name" value="contact" />
-                    <input name="name" type="text" placeholder="Name" required className="input input-bordered" />
-                    <input name="email" type="text" placeholder="Email (optional)" className="input input-bordered" />
-                    <input name="message" type="text" placeholder="Message" required className="input input-bordered" />
-                    <button className="btn btn-primary" type="submit" disabled={status === 'pending'}>
-                        Submit
-                    </button>
-                    {status === 'ok' && (
-                        <div className="alert alert-success">
-                            Submitted!
-                        </div>
-                    )}
-                    {status === 'error' && (
-                        <div className="alert alert-error">
-                            {error}
-                        </div>
-                    )}
-                </form>
-
+        name="contact"
+        onSubmit={handleFormSubmit}
+        className="text-black flex flex-col gap-3 align-center"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          required
+          className="input input-bordered"
+        />
+        <input
+          name="email"
+          type="text"
+          placeholder="Email (optional)"
+          className="input input-bordered"
+        />
+        <input
+          name="message"
+          type="text"
+          placeholder="Message"
+          required
+          className="input input-bordered"
+        />
+        <button
+          className="btn btn-primary"
+          type="submit"
+          disabled={status === "pending"}
+        >
+          Submit
+        </button>
+        {status === "ok" && (
+          <div className="alert alert-success">Submitted!</div>
+        )}
+        {status === "error" && <div className="alert alert-error">{error}</div>}
+      </form>
     </div>
   );
 }
