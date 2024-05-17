3022af41d05170833fdde85fb9971d5484e4537d
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-17 20:40:02 +0500

    chnges

--- components/shared/contactForm.tsx
+++ components/shared/contactForm.tsx
@@ -16,95 +16,31 @@ function ContactForm() {
         <span className="text-orange">info@e3.ventures</span>
       </HeadingDescription>
       
-      <form name="contact" method="POST" data-netlify="true" className="flex gap-7 flex-col">
-      <input type="hidden" name="form-name" value="contact" />
-
-      <p>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
-      </p>
-      <p>
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" />
-      </p>
-      <p>
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-
+      <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" className="flex gap-7 flex-col">
+        <input type="hidden" name="form-name" value="contact" />
+        <p className="hidden">
+          <label>
+            Don’t fill this out if you’re human: <input name="bot-field" />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" required />
+        </p>
+        <p>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" required></textarea>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
 
 export default ContactForm;
-
-const TextField = ({
-  title,
-  placeholder,
-  type,
-  name,
-}: {
-  title: string;
-  placeholder: string;
-  type: string;
-  name:string
-}) => {
-  return (
-    <div className="flex flex-col col-span-1 gap-5">
-      <p className="text-themeDarkGreen text-sm font-semibold mb-[-9px]">
-        {title}
-      </p>
-      <input
-        type={type}
-        placeholder={placeholder}
-        name={name}
-        className="border-b border-themeDarkGreen outline-none rounded-lg text-black text-sm focus:border-0"
-      />
-    </div>
-  );
-};
-
-const PhoneField = ({
-  title,
-  placeholder,
-  name,
-
-}: {
-  title: string;
-  placeholder: string;
-  name: string;
-}) => {
-  // Sample country codes list, you can replace it with your own list
-  const countryCodes = [
-    { code: "+966" },
-    { code: "+20" },
-    // Add more country codes as needed
-  ];
-
-  return (
-    <div className="flex flex-col col-span-1 gap-5">
-      <p className="text-themeDarkGreen text-sm font-semibold mb-[-9px]">
-        {title}
-      </p>
-      <div className="flex items-center rounded-lg bg-white">
-        <select className="border-0 bg-transparent  border-themeDarkGreen outline-none rounded-l-lg text-black text-sm">
-          {countryCodes.map((country, index) => (
-            <option key={index} value={country.code}>
-              ({country.code})
-            </option>
-          ))}
-        </select>
-        <input
-          type="text"
-          placeholder={placeholder}
-          name={name}
-          className="border-none bg-transparent border-themeDarkGreen outline-none focus:outline-none rounded-r-lg text-black text-sm focus:border-0"
-        />
-      </div>
-    </div>
-  );
-};
