28cb138b435fb4ceb686a826a0efbad2285e276c
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-18 00:51:33 +0500

    chnges

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -15,7 +15,7 @@ function ContactForm() {
       setError("error");
       const myForm = event.target;
       const formData = new FormData(myForm);
-      const res = await fetch("/forms.html", {
+      const res = await fetch("/shared/forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData.toString()),
