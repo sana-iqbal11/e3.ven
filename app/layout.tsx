d8e0e2d17cc21ea3ed0b7d6fd5feffa220a6e6e4
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-20 12:19:50 +0500

    chnges

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,48 @@
+"use client"
 import "../styles/globals.css";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import Footer from "@/components/base/footer";
+import { useEffect } from "react";
 
 const sora = Sora({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "E3.Ventures | Saudi Venture Builder | Startup Studio in KSA",
-  description:
-    "E3 Ventures: Your trusted partner leads the way in Saudi Arabia. Our venture studio supports startups with funding and mentorship. Together, let's build a thriving startup ecosystem!",
-};
+// export const metadata: Metadata = {
+//   title: "E3.Ventures | Saudi Venture Builder | Startup Studio in KSA",
+//   description:
+//     "E3 Ventures: Your trusted partner leads the way in Saudi Arabia. Our venture studio supports startups with funding and mentorship. Together, let's build a thriving startup ecosystem!",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    const canonicalLink = document.createElement('link');
+    canonicalLink.rel = 'canonical';
+    canonicalLink.href = window.location.href; // Dynamic URL
+    document.head.appendChild(canonicalLink);
+
+    return () => {
+      // Cleanup if necessary
+      document.head.removeChild(canonicalLink);
+    };
+  }, []);
+
+
   return (
     <html lang="en">
       <head>
-        <meta name="robots" content="noindex,nofollow" />
-        <link
-          rel="canonical"
-          href="https://deploy-preview-1--wwwe3ventures.netlify.app/"
+      <title>
+          Startup Acceleration Program in Saudi Market | Accelerators in KSA |
+          E3.Ventures
+        </title>
+        <meta
+          name="description"
+          content="Expand your startup in Saudi market with our 3-month acceleration program. E3 Ventures supports MENA entrepreneurs with essential resources. Apply to join our ecosystem now!"
         />
+        <meta name="robots" content="index,follow"/>
       </head>
       <body className={`relative ${sora.className} bg-darkBlue`}>
         {children}
